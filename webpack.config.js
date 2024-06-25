40f7ed5210a1b4892679934af83e1f834e972e89
ParkSohyunee <sohyunee016@gmail.com>
2024-06-25 15:47:51 +0900

    chore: 스타일링을 위한 postcss-loader, css-loader, style-loader 설치 및 웹팩 환경 설정

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
           presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
   devServer: {
